c002f50d9d033a34d299d180489aeb4424d01de4
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2023-02-11 17:57:59 -0800

    iterate String and convert char to Binary
    Reads a string from a console. Prints a new line, followed by each character of input on a separate line, Formatted as <char> = <binary value>.

--- main.java
+++ main.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    static void convertBinary(String string) {
+        System.out.println("The result:");
+        for (int char_ = 0; char_ < string.length(); char_++) {
+            char currentChar = string.charAt(char_);
+            int ascii = (int)currentChar;
+            String binaryString = Integer.toBinaryString(ascii);
+            System.out.println(currentChar + " = " + String.format("%7s", binaryString).replace(' ',  '0'));
+        }
+    }    
+    
     public static void main(String[] args) {
         System.out.println("Input string:");
         Scanner in = new Scanner(System.in);
         String string = in.nextLine();
-        System.out.print(string.replace("", " "));
+        convertBinary(string);
     }
 }
